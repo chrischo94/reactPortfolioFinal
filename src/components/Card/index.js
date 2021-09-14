4a9dcda716ccc562869135741a784af144f4a71b
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-09-14 17:42:09 +0900

    photo of me fixing component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,19 @@
-<Container>
-  <Row>
-    
-    <Col xs={6} md={4} class="justify-right">
-      <Image src="../../assets/images/me.JPG" roundedCircle />
-    </Col>
-    
-  </Row>
-</Container>
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+function Card() {
+    return (
+        <Container>
+            <Row>
+                
+                <Col xs={6} md={4} class="justify-right">
+                <Image src="../../assets/images/me.JPG" roundedCircle />
+                </Col>
+                
+            </Row>
+        </Container>
+    )
+}
+
+export default Card;
