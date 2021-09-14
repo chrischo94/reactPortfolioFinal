c0052c15b845ac23feb00e9ec98eb89f8bfddde8
chrischo94 <82658603+chrischo94@users.noreply.github.com>
2021-09-14 17:26:55 +0900

    photo of me update LINK

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,7 @@
   <Row>
     
     <Col xs={6} md={4} class="justify-right">
-      <Image src="./././public/me.JPG" roundedCircle />
+      <Image src="../../assets/images/me.JPG" roundedCircle />
     </Col>
     
   </Row>
